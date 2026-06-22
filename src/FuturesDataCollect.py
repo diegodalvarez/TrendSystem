ac8360dabf78b35f599499cb99d7a17c112a9cba
diegodalvarez <48641554+diegodalvarez@users.noreply.github.com>
2026-06-22 10:41:13 -0400

    adding more futures

--- src/FuturesDataCollect.py
+++ src/FuturesDataCollect.py
@@ -16,17 +16,59 @@ class FuturesDataCollect:
         self.src_path  = os.getcwd()
         self.root_path = os.path.abspath(os.path.join(self.src_path, ".."))
         self.data_path = os.path.join(self.root_path, "data")
-    
-    def get_fut_px(
+        self.px_path   = os.path.join(self.data_path, "FuturesData")
+        
+        if os.path.exists(self.px_path) == False: os.makedirs(self.px_path)
+        
+    def _get_left_over_futures(
             self, 
-            fut_path: str, 
-            fx_path : str, 
-            verbose : bool = True) -> None: 
+            path: str,
+            df  : pd.DataFrame) -> pd.DataFrame: 
+        
+        read_path    = os.path.join(path, "LeftOverFutures.xlsx")
+        sheet_names  = pd.ExcelFile(path_or_buffer = read_path).sheet_names
+        df_tmp_guide = (df.assign(
+            tmp = lambda x: x.ticker.str.split("1").str[0].str.strip()).
+            query("tmp == @sheet_names").
+            sort_values("ticker").
+            query("ticker != ['SM1 Comdty', 'QC1 Comdty']").
+            rename(columns = {"tmp": "root_ticker"}))
+    
+        df_out = pd.DataFrame()
+    
+        for sheet_name in sheet_names: 
+            
+            df_tmp = (pd.read_excel(
+                io = read_path, sheet_name = sheet_name)
+                [["Date", "Last Px"]].
+                rename(columns = {"Last Px": "px"}).
+                assign(root_ticker = sheet_name).
+                merge(
+                    right = df_tmp_guide, 
+                    how   = "inner", 
+                    on    = ["root_ticker"]).
+                assign(px = lambda x: x.px.astype(float)))
+            
+            df_out = pd.concat([df_out, df_tmp])
+
+        return df_out
+    
+    def get_raw_fut_px(
+            self, 
+            fut_path     : str, 
+            fx_path      : str, 
+            leftover_path: str,
+            verbose      : bool = True) -> None: 
+        
+        '''
+        Gets the raw futures data from all the data sources, combines them,
+        then adjusts each future by the currency rate to get them in USD
+        '''
         
         if verbose: 
             print("Collecting Futures Data")
             
-        out_path = os.path.join(self.data_path, "FuturesPX.parquet")
+        out_path = os.path.join(self.px_path, "RawFuturesPX.parquet")
         
         if os.path.exists(out_path) == True: 
             if verbose: 
@@ -42,6 +84,20 @@ class FuturesDataCollect:
             io = ticker_path, sheet_name = "fut_guide")
             [["Front", "currency"]].
             rename(columns = {"Front": "ticker"}))
+        
+        keep_tickers = (df_ticker_guide.
+                        ticker.
+                        drop_duplicates().
+                        sort_values().
+                        to_list())
+        
+        df_extra = (self._get_left_over_futures(
+            leftover_path, df_ticker_guide).
+            rename(columns = {
+                "Date": "date",
+                "px"  : "PX_LAST"}).
+            assign(date = lambda x: pd.to_datetime(x.date)).
+            drop(columns = ["root_ticker", "currency"]))
         
         fx_names = ([
             fx + "USD Curncy" for fx in 
@@ -85,8 +141,9 @@ class FuturesDataCollect:
             rename(columns = {"security": "ticker"}))
         
         df_raw_px = (pd.concat([
-            df_vix, df_tmp, df_other]).
-            dropna())
+            df_vix, df_tmp, df_other, df_extra]).
+            dropna().
+            query("ticker == @keep_tickers"))
         
         fx_map       = df_ticker_guide.set_index("ticker").currency.to_dict()
         good_tickers = list(fx_map.keys())
@@ -107,8 +164,58 @@ class FuturesDataCollect:
             
         df_out.to_parquet(path = out_path, engine = "pyarrow")
         
-fx_path     = r"C:\Users\Diego\Desktop\WeekyNotebooks\20260609LastBBG"
-fut_path    = r"C:\Users\Diego\Desktop\WeekyNotebooks\20260609LastBBG\Prices"
-
-fut_collect = FuturesDataCollect()
-fut_collect.get_fut_px(fut_path, fx_path)
+    def prep_fut_px(self, verbose: bool = True) -> None: 
+        
+        '''
+        Prepares futures data in USD terms as well small formatting adjustments
+        '''
+        
+        out_path = os.path.join(self.px_path, "PrepFuturesPX.parquet")
+        if os.path.exists(out_path) == True: 
+            if verbose: 
+                print("Already have data\n")
+                
+            return None
+        
+        if verbose: 
+            print("Cleaning futures data")
+        
+        in_path  = os.path.join(self.px_path, "RawFuturesPX.parquet")
+        end_date = (pd.read_parquet(
+            path = in_path, engine = "pyarrow")
+            [["ticker", "date"]].
+            groupby(["ticker"]).
+            agg("max").
+            date.
+            min())
+        
+        df_out = (pd.read_parquet(
+            path = in_path, engine = "pyarrow").
+            assign(ticker = lambda x: (
+                x.ticker
+                .str.lower()
+                .str.replace(" 1", "1")
+                .str.strip()
+                .str.replace(" ", "_"))).
+            query("date < @end_date").
+            pivot(index = "date", columns = "ticker", values = "adj_val").
+            reset_index().
+            melt(id_vars = "date", var_name = "ticker", value_name = "px_val").
+            dropna())
+        
+        if verbose: 
+            print("Saving data\n")
+        
+        df_out.to_parquet(path = out_path, engine = "pyarrow")
+        
+def main() -> None: 
+        
+    fx_path    = r"C:\Users\Diego\Desktop\WeekyNotebooks\20260609LastBBG"
+    fut_path   = r"C:\Users\Diego\Desktop\WeekyNotebooks\20260609LastBBG\Prices"
+    extra_path = r"C:\Users\Diego\Desktop\WeekyNotebooks\20260618FuturesVolsTmp"
+    
+    fut_collect = FuturesDataCollect()
+    fut_collect.get_raw_fut_px(fut_path, fx_path, extra_path)
+    fut_collect.prep_fut_px()
+    
+if __name__ == "__main__": main()
