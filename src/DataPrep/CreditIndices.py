d2a41fb76acf7ce11868a93bb795dc59aa388728
diegodalvarez <48641554+diegodalvarez@users.noreply.github.com>
2026-08-15 11:21:32 -0400

    PrepCDSData

--- src/DataPrep/CreditIndices.py
+++ src/DataPrep/CreditIndices.py
@@ -10,6 +10,12 @@ import pandas as pd
 import yfinance as yf
 import datetime as dt
 
+from sklearn.cluster import KMeans
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler
+
+
+
 class CreditData:
     
     def __init__(self) -> None: 
@@ -27,6 +33,9 @@ class CreditData:
         self.credit_path = os.path.join(self.data_path, "CreditData")
         if not os.path.exists(self.credit_path):
             os.makedirs(self.credit_path)
+            
+        self.n_clusters   = 5
+        self.random_state = 42
         
     def collect_raw_cds_data(self, verbose: bool = True) -> None: 
         
@@ -140,11 +149,103 @@ class CreditData:
         if verbose: print("Saving data\n")
         df_credit_index.to_parquet(path = out_path, engine = "pyarrow")
         
+    def _slice_ends(self, df: pd.DataFrame) -> pd.DataFrame: 
+        
+        date_type = df.name[-1]
+        if date_type == "max":
+            
+            df_out = (df
+                    .loc[lambda x: x.value == x.value.min()]
+                    .head(1))
+            
+        elif date_type == "min":
+            
+            df_out = (df
+                      .loc[lambda x: x.value == x.value.max()]
+                      .head(1))
+
+        return df_out
+        
+    def prep_cds_data(self, slice_year: int = 2012, verbose: bool = True) -> None: 
+        
+        if verbose: 
+            print("Prepping CDS Data")
+            
+        out_path = os.path.join(self.credit_path, "PrepCDS.parquet")
+        
+        if os.path.exists(out_path):
+            if verbose: print("Already have data\n")
+            return None
+        
+        ticker_path = os.path.join(self.data_path, "TickerGuide.xlsx")
+        df_ticker   = (pd
+                .read_excel(io = ticker_path, sheet_name = "credit")
+                .loc[lambda x: x.Active == True]
+                .loc[lambda x: x.AssetClass == "CreditDefaultSwap"]
+                [["Ticker", "SeriesType", "SecurityGroup"]]
+                .rename(columns = {"Ticker": "security"}))
+        
+        cred_path = os.path.join(self.credit_path, "RawCDS.parquet")
+    
+        df_raw = (pd
+                .read_parquet(path = cred_path, engine = "pyarrow")
+                .merge(right = df_ticker, how = "inner", on = ["security"])
+                .loc[lambda x: x.variable == "PX_LAST"]
+                [["date", "SeriesType", "SecurityGroup","value"]])
+        
+        df_date_slicer = (df_raw
+                .drop(columns = ["value"])
+                .groupby(["SecurityGroup", "SeriesType"])
+                ["date"]
+                .agg(["min", "max"])
+                .reset_index()
+                .melt(id_vars = ["SecurityGroup", "SeriesType"])
+                .groupby(["SecurityGroup", "variable"])
+                .apply(self._slice_ends)
+                .reset_index()
+                [["SecurityGroup", "variable", "value"]]
+                .assign(variable = lambda x: x.variable + "_date")
+                .pivot(index = "SecurityGroup", columns = "variable", values = "value"))
+
+        df_sliced_wider = (df_raw
+                .merge(right = df_date_slicer, how = "inner", on = ["SecurityGroup"])
+                .loc[lambda x: (x.min_date <= x.date) & (x.date <= x.max_date)]
+                .assign(year = lambda x: x.date.dt.year)
+                .loc[lambda x: x.year >= slice_year]
+                .drop(columns = ["year"])                
+                .drop(columns = ["min_date", "max_date"])
+                .pivot(index = ["date", "SecurityGroup"], columns = "SeriesType", values = "value"))
+        
+        df_spread_wider = (df_sliced_wider
+                .reset_index()
+                .pivot(index = "date", columns = "SecurityGroup", values = "spread")
+                .interpolate(method = "time"))
+        
+        df_spread = (df_spread_wider
+                .reset_index()
+                .melt(id_vars = "date", value_name = "spread")
+                .dropna())
+        
+        df_out = (df_sliced_wider
+                .reset_index()
+                .drop(columns = ["spread"])
+                .merge(right = df_spread, how = "outer", on = ["date", "SecurityGroup"]))
+        
+        if verbose: print("Saving data\n")
+        df_out.to_parquet(path = out_path, engine = "pyarrow")
+        
 def main() -> None: 
         
     credit_data = CreditData()
+ 
+    # Raw
+    '''
     credit_data.collect_raw_cds_data()
     credit_data.collect_raw_credit_etf()
     credit_data.collect_raw_credit_indices()
+    '''
+    
+    # Prep
+    credit_data.prep_cds_data()
     
 if __name__ == "__main__": main()
